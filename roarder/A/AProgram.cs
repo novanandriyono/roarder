f22b07de6cb442f2047b594ae0b7988db9b2b511
novanandriyono <evelynradtya@gmail.com>
2019-04-05 00:25:59 +0700

    add new files

---
+++ roarder/A/AProgram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+namespace Roarder.A
+{
+    abstract class AProgram
+    {
+        protected AProgram SetProgram(string[] args) => this.SetAProgram(args);
+        private AProgram SetAProgram(string[] args) {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, arg) => { if (arg.Name.StartsWith("Newtonsoft")) return Assembly.Load(Properties.Resources.Newtonsoft_Json); return null; };
+            return typeof(Exe).Equals(new Exe(args)) ?this:null;
+        }
+    }
+}
